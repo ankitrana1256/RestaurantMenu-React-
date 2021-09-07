ae5d03f9f4fe3e70cd9a8eee8a8f39f97816e56f
ankitrana1256 <76605774+ankitrana1256@users.noreply.github.com>
2021-09-07 21:28:14 +0530

    All Button

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -6,15 +6,22 @@ import Navbar from './Navbar';
 const uniqueList = [
     ...new Set(Menu.map((curElem) => {
     return curElem.category;
-}))]
+})),
+"All"]
 console.log(uniqueList);
 
 const Restaurant = () => {
+
 
     const [menuData, setMenuData] = React.useState(Menu);
     const [menuList, setMenuList] = React.useState(uniqueList);
 
     const filterItem = (category) => {
+
+        if(category === "All"){
+            setMenuData(Menu);
+            return;
+        }
         const updatedList = Menu.filter((curElem)=>{
             return curElem.category == category;
         });
